6b787914635bdd50e746cefa3a8ca860914f7e99
YasminMuntaserN <161508263+YasminMuntaserN@users.noreply.github.com>
2024-09-19 15:20:48 +0300

    feat: add StartScreen component

---
+++ src/components/StartScreen.js
@@ -0,0 +1,11 @@
+function StartScreen() {
+  return (
+    <div className="start">
+      <h2>Welcome to The React Quiz ! </h2>
+      <p>num</p>
+      <button>Lets Start</button>
+    </div>
+  );
+}
+
+export default StartScreen;
